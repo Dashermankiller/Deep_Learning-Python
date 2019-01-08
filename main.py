7dd4f4bd637e6eac3cde1bfcc5b5b19849d6eba6
root <root@DESKTOP-0OPUU9O.localdomain>
2019-01-08 01:03:52 -0500

    Update

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from sklearn import tree
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 # 1== Smooth
 # 0== bumpy
 features =[[140,1],[130,1],[150,0],[170,0]]
 labels=[0,0,1,1]
 clf=tree.DecisionTreeClassifier()
 clf=clf.fit(features,labels)
-print (clf.predict([[130,1]]))
+lb=int(input("LB: "))
+a=int(input("Smooth: "))
+prediction=clf.predict([[lb,a]])
+if prediction ==0:
+    img=mpimg.imread('Apple.jpg')
+    imgplot = plt.imshow(img)
+    plt.show()
+elif prediction==1:
+    img=mpimg.imread('Orange.jpg')
+    imgplot = plt.imshow(img)
+    plt.show()
 
 # 1 == Orange
 # 0== Apple
